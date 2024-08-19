13ae476888b012cc97f5e5b19dfd75e7ac4a7777
Çağla Şen <caglasenn1@gmail.com>
2024-08-19 22:02:30 +0300

    userClientService updated in eticket-service

--- eticket-service/src/main/java/com/ftbootcamp/eticketservice/client/user/UserClientService.java
+++ eticket-service/src/main/java/com/ftbootcamp/eticketservice/client/user/UserClientService.java
@@ -1,6 +1,9 @@
 package com.ftbootcamp.eticketservice.client.user;
 
 import com.ftbootcamp.eticketservice.client.user.dto.UserDetailsResponse;
+import com.ftbootcamp.eticketservice.dto.response.GenericResponse;
+import com.ftbootcamp.eticketservice.dto.response.GenericResponseConstants;
+import com.ftbootcamp.eticketservice.exception.ETicketException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +14,13 @@ public class UserClientService {
     private final UserClient userClient;
 
     public UserDetailsResponse getUserById(Long id) {
-        return userClient.getUserById(id).getData();
+        GenericResponse<UserDetailsResponse> response = userClient.getUserById(id);
+
+        if (response.getStatus().equals(GenericResponseConstants.FAILED)) {
+            throw new ETicketException(response.getMessage(), "Failed to get user by id");
+        }
+
+        return response.getData();
     }
 
 }
